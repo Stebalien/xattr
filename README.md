77be6e3e7c6837561c0ff5ead9fda6b3a9e53735
Steven Allen <steven@stebalien.com>
2015-07-10 12:05:05 -0400

    Document

---
+++ README.md
@@ -0,0 +1,8 @@
+xattr
+=====
+
+[![Build Status](https://travis-ci.org/Stebalien/xattr.svg?branch=master)](https://travis-ci.org/Stebalien/xattr)
+
+A small library for setting, getting, and listing extended attributes.
+
+Documentation: https://stebalien.github.com/xattr/xattr/
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ use std::path::Path;
 
 pub use sys::XAttrs;
 
+/// Get an extended attribute for the specified file.
 pub fn get<N, P>(path: P, name: N) -> io::Result<Vec<u8>> 
     where P: AsRef<Path>,
           N: AsRef<OsStr>
@@ -18,6 +19,7 @@ pub fn get<N, P>(path: P, name: N) -> io::Result<Vec<u8>>
     sys::get_path(path.as_ref(), name.as_ref())
 }
 
+/// Set an extended attribute on the specified file.
 pub fn set<N, P>(path: P, name: N, value: &[u8]) -> io::Result<()> 
     where P: AsRef<Path>,
           N: AsRef<OsStr>
@@ -25,6 +27,7 @@ pub fn set<N, P>(path: P, name: N, value: &[u8]) -> io::Result<()>
     sys::set_path(path.as_ref(), name.as_ref(), value)
 }
 
+/// Remove an extended attribute from the specified file.
 pub fn remove<N, P>(path: P, name: N) -> io::Result<()> 
     where P: AsRef<Path>,
           N: AsRef<OsStr>
@@ -32,34 +35,45 @@ pub fn remove<N, P>(path: P, name: N) -> io::Result<()>
     sys::remove_path(path.as_ref(), name.as_ref())
 }
 
+/// List extended attributes attached to the specified file.
+///
+/// Note: this may not list *all* attributes. Speficially, it definitely won't list any trusted
+/// attributes unless you are root and it may not list system attributes.
 pub fn list<P>(path: P) -> io::Result<XAttrs>
     where P: AsRef<Path>
 {
     sys::list_path(path.as_ref())
 }
 
-pub unsafe trait FileExt: AsRawFd {
+pub trait FileExt: AsRawFd {
+    /// Get an extended attribute for the specified file.
     fn get_xattr<N>(&self, name: N) -> io::Result<Vec<u8>> 
         where N: AsRef<OsStr>
     {
         sys::get_fd(self.as_raw_fd(), name.as_ref())
     }
 
+    /// Set an extended attribute on the specified file.
     fn set_xattr<N>(&self, name: N, value: &[u8]) -> io::Result<()> 
         where N: AsRef<OsStr>
     {
         sys::set_fd(self.as_raw_fd(), name.as_ref(), value)
     }
 
+    /// Remove an extended attribute from the specified file.
     fn remove_xattr<N>(&self, name: N) -> io::Result<()> 
         where N: AsRef<OsStr>
     {
         sys::remove_fd(self.as_raw_fd(), name.as_ref())
     }
 
+    /// List extended attributes attached to the specified file.
+    ///
+    /// Note: this may not list *all* attributes. Speficially, it definitely won't list any trusted
+    /// attributes unless you are root and it may not list system attributes.
     fn list_xattr(&self) -> io::Result<XAttrs> {
         sys::list_fd(self.as_raw_fd())
     }
 }
 
-unsafe impl FileExt for File { }
+impl FileExt for File { }

850e96ca225163a54ed5138f004bf7c68f07e87a
Steven Allen <steven@stebalien.com>
2015-07-10 12:09:22 -0400

    test by path.

--- tests/main.rs
+++ tests/main.rs
@@ -7,7 +7,7 @@ use tempfile::NamedTempFile;
 
 #[test]
 #[cfg(target_os = "linux")]
-fn test() {
+fn test_fd() {
     // Only works on "real" filesystems.
     let tmp = NamedTempFile::new_in("/var/tmp").unwrap();
     assert!(tmp.get_xattr("user.test").is_err());
@@ -20,4 +20,21 @@ fn test() {
     tmp.remove_xattr("user.test").unwrap();
     assert!(tmp.get_xattr("user.test").is_err());
     assert_eq!(tmp.list_xattr().unwrap().next(), None);
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_path() {
+    // Only works on "real" filesystems.
+    let tmp = NamedTempFile::new_in("/var/tmp").unwrap();
+    assert!(xattr::get(tmp.path(), "user.test").is_err());
+    assert_eq!(xattr::list(tmp.path(), ).unwrap().next(), None);
+
+    xattr::set(tmp.path(), "user.test", b"my test").unwrap();
+    assert_eq!(xattr::get(tmp.path(), "user.test").unwrap(), b"my test");
+    assert_eq!(&xattr::list(tmp.path(), ).unwrap().next().unwrap(), "user.test");
+
+    xattr::remove(tmp.path(), "user.test").unwrap();
+    assert!(xattr::get(tmp.path(), "user.test").is_err());
+    assert_eq!(xattr::list(tmp.path(), ).unwrap().next(), None);
 }

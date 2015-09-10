ff475e259089900b99982857e6cc4fb11935ab66
Steven Allen <steven@stebalien.com>
2015-09-10 10:48:14 -0400

    Update docs for 16daf0159f672e5ca19a39c4ec0ddbd54edd32e3
    Fix iterating over xattrs

--- implementors/core/iter/trait.Iterator.js
+++ implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['xattr'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='xattr/struct.XAttrs.html' title='xattr::XAttrs'>XAttrs</a>",];
+implementors['xattr'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='xattr/struct.XAttrs.html' title='xattr::XAttrs'>XAttrs</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

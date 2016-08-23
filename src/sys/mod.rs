501dbb481c1c4b1c6932dc1ef3b3c88f158d46eb
Steven Allen <steven@stebalien.com>
2016-08-22 21:37:27 -0400

    Add dummy interface for unsupported platforms.
    This just makes it easier to write portable code (of coarse, the code will fail
    with runtime errors on unsupported platforms but that can always happen...).

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1,4 +1,3 @@
-
 #[cfg(any(target_os = "macos", target_os = "linux"))]
 mod linux_macos;
 
@@ -10,3 +9,18 @@ mod bsd;
 
 #[cfg(any(target_os = "freebsd", target_os = "netbsd"))]
 pub use self::bsd::*;
+
+#[cfg(not(any(target_os = "freebsd", target_os = "netbsd", target_os = "macos", target_os = "linux")))]
+mod unsupported;
+
+#[cfg(not(any(target_os = "freebsd", target_os = "netbsd", target_os = "macos", target_os = "linux")))]
+pub use self::unsupported::*;
+
+/// A constant indicating whether or not the target platform is supported.
+///
+/// To make programmer's lives easier, this library builds on all platforms.
+/// However, all function calls on unsupported platforms will return io::Errors.
+pub const SUPPORTED_PLATFORM: bool = cfg!(any(target_os = "freebsd",
+                                              target_os = "netbsd",
+                                              target_os = "macos",
+                                              target_os = "linux"));

f6802d775b5e7d0250eed22b27476e9235190182
Steven Allen <steven@stebalien.com>
2016-08-22 21:45:30 -0400

    Add feature to turn off support for unsupported platforms.

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -10,16 +10,19 @@ mod bsd;
 #[cfg(any(target_os = "freebsd", target_os = "netbsd"))]
 pub use self::bsd::*;
 
-#[cfg(not(any(target_os = "freebsd", target_os = "netbsd", target_os = "macos", target_os = "linux")))]
+#[cfg(all(feature = "unsupported", not(any(target_os = "freebsd", target_os = "netbsd", target_os = "macos", target_os = "linux"))))]
 mod unsupported;
 
-#[cfg(not(any(target_os = "freebsd", target_os = "netbsd", target_os = "macos", target_os = "linux")))]
+#[cfg(all(feature = "unsupported", not(any(target_os = "freebsd", target_os = "netbsd", target_os = "macos", target_os = "linux"))))]
 pub use self::unsupported::*;
 
 /// A constant indicating whether or not the target platform is supported.
 ///
 /// To make programmer's lives easier, this library builds on all platforms.
 /// However, all function calls on unsupported platforms will return io::Errors.
+///
+/// Note: If you would like compilation to simply fail on unsupported platforms,
+/// turn of the `unsupported` feature.
 pub const SUPPORTED_PLATFORM: bool = cfg!(any(target_os = "freebsd",
                                               target_os = "netbsd",
                                               target_os = "macos",

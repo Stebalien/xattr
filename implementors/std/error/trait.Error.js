2102c3f19c4778dadd5615c590d0e43852d72213
Steven Allen <steven@stebalien.com>
2017-07-16 00:18:13 -0700

    Update docs for 8d32e8784e6315003c724744113af4d75921bcf0
    Release 0.2.0
    
    * Return Result<Option<...>> instead of just results on get.

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["libc"] = [];
-implementors["xattr"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='trait std::error::Error'>Error</a> for <a class='struct' href='xattr/struct.UnsupportedPlatformError.html' title='struct xattr::UnsupportedPlatformError'>UnsupportedPlatformError</a>",];
+implementors["xattr"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"xattr/struct.UnsupportedPlatformError.html\" title=\"struct xattr::UnsupportedPlatformError\">UnsupportedPlatformError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

bfa74743698e80be6353cae0ee07ac162c6a4f6c
Steven Allen <steven@stebalien.com>
2018-03-31 17:21:54 -0700

    Update docs for 62a1153a65e8ed8132865dddf343cc7656c87749
    move documentation to docs.rs

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];
-implementors["xattr"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"xattr/struct.UnsupportedPlatformError.html\" title=\"struct xattr::UnsupportedPlatformError\">UnsupportedPlatformError</a>",];
+implementors["xattr"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"xattr/struct.UnsupportedPlatformError.html\" title=\"struct xattr::UnsupportedPlatformError\">UnsupportedPlatformError</a>",synthetic:false,types:["xattr::error::UnsupportedPlatformError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

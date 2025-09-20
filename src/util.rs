17882a3541d5e38c7926ac3c6f20c636fda06172
kimono-koans <32370782+kimono-koans@users.noreply.github.com>
2025-09-20 15:42:12 -0500

    Initial commit

--- src/util.rs
+++ src/util.rs
@@ -14,16 +14,11 @@ pub fn extract_noattr(result: io::Result<Vec<u8>>) -> io::Result<Option<Vec<u8>>
 pub fn allocate_loop<E, F: FnMut(&mut [u8]) -> Result<usize, E>>(mut f: F) -> io::Result<Vec<u8>>
 where
     io::Error: From<E>,
-{
-    let mut vec: Vec<u8> = Vec::new();
-    loop {
-        let ret = f(&mut [])?;
-        vec.resize(ret, 0);
+{    loop {
+        let mut vec: Vec<u8> = Vec::new();
 
         match f(&mut vec) {
             Ok(size) => {
-                vec.truncate(size);
-                vec.shrink_to_fit();
                 return Ok(vec);
             }
 

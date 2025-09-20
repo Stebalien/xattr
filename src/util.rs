1ec2f591a860e8c54c89ae44795ec85b4ada4f99
Steven Allen <steven@stebalien.com>
2025-09-20 15:00:36 -0700

    feat: guess the max xattr size once (#77)
    Instead of always allocating in a loop, guess that the extended
    attribute value is less than 256 bytes and try once.
    
    - In the best case when extended attributes are small, this will save us
    a syscall.
    - In the worst case when they're larger than 256 bytes, we'll make 3
    syscalls instead of 2.
    
    fixes #75

--- src/util.rs
+++ src/util.rs
@@ -15,6 +15,23 @@ pub fn allocate_loop<E, F: FnMut(&mut [u8]) -> Result<usize, E>>(mut f: F) -> io
 where
     io::Error: From<E>,
 {
+    // Try assuming the variable is less than 256. We do this on the stack and copy to avoid double
+    // allocation.
+    const INITIAL_BUFFER_SIZE: usize = 256;
+    {
+        let mut buf = [0u8; INITIAL_BUFFER_SIZE];
+        match f(&mut buf[..]) {
+            Ok(len) => return Ok(buf[..len].to_vec()),
+            Err(e) => {
+                let err: io::Error = e.into();
+                if err.raw_os_error() != Some(crate::sys::ERANGE) {
+                    return Err(err);
+                }
+            }
+        }
+    }
+
+    // If that fails, enter the allocation loop.
     let mut vec: Vec<u8> = Vec::new();
     loop {
         let ret = f(&mut [])?;

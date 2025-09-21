8b183b3580c466d4b035f330671eba50ae8161e3
Steven Allen <steven@stebalien.com>
2025-09-20 19:31:33 -0700

    test: test listing more than 4096 bytes of extended attributes
    fixes #84

--- tests/main.rs
+++ tests/main.rs
@@ -2,7 +2,7 @@ use std::collections::BTreeSet;
 use std::ffi::OsStr;
 use xattr::FileExt;
 
-use tempfile::{tempfile, NamedTempFile};
+use tempfile::{tempfile, tempfile_in, NamedTempFile};
 
 #[test]
 #[cfg(any(
@@ -157,6 +157,37 @@ fn test_multi() {
     .iter()
     .cloned()
     .collect();
+
+    for it in &items {
+        tmp.set_xattr(it, b"value").unwrap();
+    }
+    for it in tmp
+        .list_xattr()
+        .unwrap()
+        .filter(|x| x.as_bytes().starts_with(b"user."))
+    {
+        assert!(items.remove(&*it));
+    }
+    assert!(items.is_empty());
+}
+
+// This test is skipped on android because the /data/tmp filesystem doesn't support >4kib of
+// extended attributes.
+#[test]
+#[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "macos"))]
+fn test_large() {
+    use std::ffi::OsString;
+    use std::os::unix::ffi::OsStrExt;
+    // On Linux, this only works in tmpfs!
+    let tmp = if cfg!(target_os = "linux") {
+        tempfile_in("/dev/shm")
+    } else {
+        tempfile()
+    }
+    .unwrap();
+    let mut items: BTreeSet<OsString> = (0..100)
+        .map(|i| format!("user.test{i:0100}").into())
+        .collect();
 
     for it in &items {
         tmp.set_xattr(it, b"value").unwrap();
